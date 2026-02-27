d70c96dea638a840c3cf158d0ec3b10b847b8ec0
22Raghava2004 <raghavangowda2004@gmail.com>
2026-02-27 16:37:26 +0530

    lerili larila

---
+++ DiceRpller/app/src/main/java/com/example/dicerpller/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.dicerpller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+import com.example.dicerpller.ui.theme.DiceRpllerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRpllerTheme {
+                DiceRollerApp()
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+
+@Preview
+@Composable
+fun DiceRollerApp() {
+    DiceWithButtonAndImage()
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier = Modifier
+    .fillMaxSize()
+    .wrapContentSize(Alignment.Center)
+){
+
+}
